3daa070670c3a0867f147faf1417b4ab43f627a5
deepamasnani <asnani.deepam@gmail.com>
2021-08-19 09:25:06 +0530

    Initialised firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+
+const config ={
+    apiKey: "AIzaSyD70rILF7S0r0bVaVReawbDIiv43Ypu_Do",
+    authDomain: "chat-web-app-52afe.firebaseapp.com",
+    projectId: "chat-web-app-52afe",
+    storageBucket: "chat-web-app-52afe.appspot.com",
+    messagingSenderId: "639286400076",
+    appId: "1:639286400076:web:5dc204c51a5358e0c9d0e9"
+  };
