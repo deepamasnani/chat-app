a619cf4fcccbe1758199cf0b646d7583db8d3d6e
deepamasnani <asnani.deepam@gmail.com>
2021-08-20 13:58:59 +0530

    added sigin page and sigin with fb/google

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
 import PrivateRoute from './components/PrivateRoute';
+import './styles/main.scss';
 import PublicRoute from './components/PublicRoute';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import './styles/main.scss';
+
+
 
 function App() {
   return <Switch>
